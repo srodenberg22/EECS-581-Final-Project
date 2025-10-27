67a2b23e6c372edd5ae35c455132e84eaafbb849
Evan Rogerson <142823470+9MotorGang@users.noreply.github.com>
2025-10-26 23:27:03 -0500

    updated player code

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 
-@export var force: float = 400.0
-@export var rotateSpeed: float = 5.0
+@export var force: float = 50.0
+@export var torque: float = .05
+var rotateSpeed = 0
 @onready var rocket: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if Input.is_action_pressed("ui_left"):
-		rotation -= rotateSpeed*delta
-	elif Input.is_action_pressed("ui_right"):
-		rotation += rotateSpeed*delta
+	if Input.is_action_pressed("ui_left") and not(Input.is_action_pressed("ui_right")) :
+		rotateSpeed -= torque*delta
+	elif Input.is_action_pressed("ui_right") and not(Input.is_action_pressed("ui_left")):
+		rotateSpeed += torque*delta
+	
+	rotation += rotateSpeed
 		
 	if Input.is_action_pressed("ui_up"):
 		velocity += Vector2.UP.rotated(rotation)*force*delta
@@ -18,4 +21,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		rocket.play("default")
 
-	move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print("Collision Detected, ending game")
+		get_tree().quit()
+			
