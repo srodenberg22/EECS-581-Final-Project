f8ed85ea7bf63f78ce136733f85cdfa9125f8078
NevanSnider <143729670+NevanSnider@users.noreply.github.com>
2025-11-08 23:53:13 -0600

    Fixed Area2D bug and made golden asteroids duplicatable

--- new-game-project/scripts/home_base.gd
+++ new-game-project/scripts/home_base.gd
@@ -1,18 +1,18 @@
 extends Node
 
-var player_in_body = false
+var player_in_base = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Ship":
-		player_in_body = true
+		player_in_base = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Ship":
-		player_in_body = false
+		player_in_base = false
 
 		
 func _process(delta):
-	if player_in_body and Input.is_action_just_pressed("interact"):
+	if player_in_base and Input.is_action_just_pressed("interact"):
 		if GameController.package_collected == true:
 			GameController.package_return(GameController.asteroid_collected)
 			print("package returned!")
