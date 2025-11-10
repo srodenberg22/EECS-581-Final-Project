8490839adfa04c5ef7194d39d499da02b3b14a77
KarstenWolter <karwolter920@gmail.com>
2025-11-09 22:34:06 -0600

    Initial commit

--- new-game-project/scripts/home_base.gd
+++ new-game-project/scripts/home_base.gd
@@ -1,10 +1,22 @@
 extends Node
 
 var player_in_base = false
+var save_manager: Node = null
+
+func _ready():
+	save_manager = get_tree().get_current_scene().get_node("SaveManager")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Ship":
 		player_in_base = true
+		save_manager.save_game(
+			body.global_position,
+			GameController.package_collected,
+			GameController.asteroid_collected
+		)
+		#Status of asteroid/package collection for save state testing
+		print("asteroid collected:", GameController.asteroid_collected )
+		print("package collected:", GameController.package_collected )
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Ship":
@@ -17,5 +29,5 @@ func _process(delta):
 			GameController.package_return(GameController.asteroid_collected)
 			print("package returned!")
 			
-		SaveManager.save_game($".".global_position, GameController.package_collected, GameController.asteroid_collected)
+		
 		
--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -5,7 +5,27 @@ extends CharacterBody2D
 @export var torque: float = .05
 var rotateSpeed = 0
 @onready var rocket: AnimatedSprite2D = $AnimatedSprite2D
+@onready var respawn_point = get_tree().get_current_scene().get_node("HomeBase/RespawnPoint")
 
+var package_collected: bool = false
+var asteroid_collected: bool = false
+
+func _ready():
+	var data = SaveManager.load_game()
+	GameController.package_collected = data["package_collected"]
+	GameController.asteroid_collected = data["asteroid_collected"]
+
+func respawn_to_base():
+	global_position = respawn_point.global_position
+	
+	velocity = Vector2.ZERO
+	rotateSpeed = 0
+	rotation = 0
+
+	var data = SaveManager.load_game()
+	GameController.package_collected = data["package_collected"]
+	GameController.asteroid_collected = data["asteroid_collected"]
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if Input.is_action_pressed("ui_left") and not(Input.is_action_pressed("ui_right")) :
@@ -23,6 +43,6 @@ func _physics_process(delta: float) -> void:
 
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		print("Collision Detected, ending game")
-		get_tree().quit()
+		print("Collision Detected, respawning...")
+		respawn_to_base()
 			
